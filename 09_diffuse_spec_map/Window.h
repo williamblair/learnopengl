176f8cbbc91bd6ede7fca7fd4831143e00e89faa
williamblair <bblair12876@gmail.com>
2019-06-04 20:14:58 -0400

    added diffuse and specular map

---
+++ 09_diffuse_spec_map/Window.h
@@ -0,0 +1,40 @@
+#ifndef _WINDOW_H_
+#define _WINDOW_H_
+
+extern "C" {
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+}
+
+#include <iostream>
+
+class Window
+{
+public:
+    Window();
+    ~Window();
+
+    bool init(int width, int height, const char *title);
+
+    void update(void);
+    bool shouldClose(void);
+
+    void setKeyHandler(GLFWkeyfun cbfun);
+    void setMouseHandler(GLFWcursorposfun cbfun);
+    void setScrollHandler(GLFWscrollfun cbfun);
+
+    bool keyIsPressed(int key);
+
+    GLFWwindow *getWindowPtr(void);
+
+private:
+    // Variables
+    GLFWwindow *m_window = NULL;
+
+    // helper functions
+    static void m_error_callback(int error, const char *description);
+};
+
+#endif
+
+
